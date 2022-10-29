85f70c4845e0f3ae58539865fb0f711a325c62ba
usseoulusa <usseoulusa@gmail.com>
2022-10-29 10:51:52 -0400

    - List feedback files with os.listdir() - Add dummy feedback files
    Feedback file is a text file in the format: title, name, date, and
    feedback.

---
+++ 2week/run.py
@@ -0,0 +1,16 @@
+'''
+#!/usr/bin/env python3
+'''
+
+
+import os
+#import requests
+
+path = "c://a_Python//a_google_course//a_final_project//2week//feedback//"
+#save_path = "c://a_Python//a_google_course//a_final_project//2week//feedback//"
+dir_list = os.listdir(path)
+
+print(dir_list)
+
+# to store files in a list
+list = []
