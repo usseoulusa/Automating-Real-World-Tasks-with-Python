d2ce7633fca81780bcb858d3de26ba195e7d58ff
usseoulusa <usseoulusa@gmail.com>
2022-10-29 00:03:44 -0400

    Iterate each image file

--- 1week/convert.py
+++ 1week/convert.py
@@ -6,4 +6,14 @@ import os
 path = "c://a_Python//a_google_course//a_final_project//1week//images"
 dir_list = os.listdir(path)
 
-print(dir_list)
+#print(dir_list)
+
+# to store files in a list
+list = []
+ 
+# dirs=directories
+for (root, dirs, file) in os.walk(path):
+    for f in file:
+        print(f)
+        #if '.tiff' in f:
+            #print(f)
