ed7645687fbd854090e06934b6a589ee3c2200c0
usseoulusa <usseoulusa@gmail.com>
2022-10-29 02:21:12 -0400

    Copy image files to new dir

--- 1week/convert.py
+++ 1week/convert.py
@@ -3,10 +3,12 @@
 '''
 
 import os
+import shutil
 from PIL import Image
 
 
 path = "c://a_Python//a_google_course//a_final_project//1week//images//"
+save_path = "c://a_Python//a_google_course//a_final_project//1week//new_images//"
 dir_list = os.listdir(path)
 
 #print(dir_list)
@@ -23,13 +25,21 @@ list = []
     b. Resize the image from 192x192 to 128x128
     c. Save the image to a new folder in .jpeg format
 '''
+
 for (root, dirs, file) in os.walk(path):
     for f in file:
-        im = Image.open(root+f)
+        new_file = "{}.jpeg".format(f)
+        shutil.copy(root+f, save_path+new_file)
+
+for (new_root, dirs, file) in os.walk(save_path):
+    for f in file:
+        #im = Image.open(new_root+f)
         #print(f'{f}.jpeg is image')
-        new_im = im.rotate(90).resize((128,128))
-        rgb_im = new_im.convert("RGB")
-        rgb_im.save(os.path.splitext(root+f)[0]+".jpeg")
-        #print(root+f)
+        #cov_im = im.convert("RGB")
+        #im.rotate(270).resize((128, 128))
+        #rgb_im = new_im.convert("RGB")
+        #im.save(new_root+f)
+        print(new_root+f)
+        #print(save_path)
         #if '.tiff' in f:
             #print(f)
