9ea537ad316bac6c6f925f7bb80a82a469b1708c
usseoulusa <usseoulusa@gmail.com>
2022-10-29 00:13:05 -0400

    Requirement comment added

--- 1week/convert.py
+++ 1week/convert.py
@@ -13,10 +13,19 @@ dir_list = os.listdir(path)
 
 # to store files in a list
 list = []
+
  
-# dirs=directories
+''' Requirement
+1. Iterate through each file in the folder
+
+2. For each file:
+    a. Rotate the image 90° clockwise
+    b. Resize the image from 192x192 to 128x128
+    c. Save the image to a new folder in .jpeg format
+'''
 for (root, dirs, file) in os.walk(path):
     for f in file:
-        print(f)
+        
+        #print(f)
         #if '.tiff' in f:
             #print(f)
